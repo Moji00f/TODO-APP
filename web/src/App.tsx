a77077bd8602fc58e4750fd98240c61311554207
Moji00f <mojtabaahmadi2008@gmail.com>
2025-11-28 21:29:12 +0330

    not important

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,9 +3,13 @@ import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
+
 // export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
-export const BASE_URL = import.meta.env.VITE_API_URL || "api" ;
+export const BASE_URL = import.meta.env.VITE_API_URL || "api";
+
+
+
 
 function App() {
     return (
@@ -18,5 +22,8 @@ function App() {
         </Stack>
     );
 }
+
+
+
 
 export default App;
